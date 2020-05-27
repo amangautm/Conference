a941160a21d60b1a458c83ce5816967a8ef6505e
Aman Gautam <iitianamangautam@gmail.com>
2020-05-28 00:39:24 +0530

    Overview

---
+++ src/main/java/com/example/conference/controllers/SessionsController.java
@@ -0,0 +1,32 @@
+package com.example.conference.controllers;
+
+import com.example.conference.models.Session;
+import com.example.conference.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list(){
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping()
+    @RequestMapping("{id}")
+    public Session get (@PathVariable Long id){
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+}
